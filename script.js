209a17b99593555ecef57e566018ba66dbf0e046
verinuszendrato2-coder <verinuszendrato2@gmail.com>
2026-08-01 13:39:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Script akan ditambahkan nanti
+// JavaScript Website
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      alert("Tombol berhasil ditekan!");
+    });
+  });
+
+});// Script akan ditambahkan nanti
